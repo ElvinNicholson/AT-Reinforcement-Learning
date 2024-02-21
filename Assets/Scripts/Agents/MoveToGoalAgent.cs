d03703c7d6535263e18f203e760505cc26884773
Elvin Nicholson <Elvin2.Nicholson@live.uwe.ac.uk>
2024-02-21 20:50:43 +0000

    Unity ML-Agents setup

---
+++ Assets/Scripts/Agents/MoveToGoalAgent.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
+
+public class MoveToGoalAgent : Agent
+{
+    [SerializeField] private Transform targetTransform;
+    [SerializeField] private float speed;
+
+    public override void OnEpisodeBegin()
+    {
+        transform.localPosition = new Vector3(0f, 0.5f, 0f);
+    }
+
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        sensor.AddObservation(transform.localPosition);
+        sensor.AddObservation(targetTransform.localPosition);
+    }
+
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+        float moveX = actions.ContinuousActions[0];
+        float moveZ = actions.ContinuousActions[1];
+
+        transform.localPosition += new Vector3(moveX, 0, moveZ) * Time.deltaTime * speed;
+    }
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        ActionSegment<float> continuousActions = actionsOut.ContinuousActions;
+        continuousActions[0] = Input.GetAxisRaw("Horizontal");
+        continuousActions[1] = Input.GetAxisRaw("Vertical");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Target"))
+        {
+            SetReward(+1f);
+            EndEpisode();
+        }
+        else if (other.gameObject.CompareTag("Wall"))
+        {
+            SetReward(-1f);
+            EndEpisode();
+        }
+    }
+}
